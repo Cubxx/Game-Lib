d17e1ccce315c18b1c5f0bf5e59fda4b5fb08a97
Cube X <shawn6304@qq.com>
2024-04-25 13:21:30 +0800

    🌈 style:

--- Barrage/js/Player.js
+++ Barrage/js/Player.js
@@ -7,61 +7,173 @@ function Player(x, y) {
     this.generate_time = 0;
     this.no_died;
     var bomb_time = 180,
-        res_time, wipe_time,
+        res_time,
+        wipe_time,
         lives = this.lives,
         score = this.score,
         bombs = this.bombs;
-    this.event = function () { //修改属性值 lives score bombs //闭包
-        if (arguments[0] || arguments[0] === 0) { lives += arguments[0]; this.lives = lives; if (arguments[3]) { return this.lives } }
-        if (arguments[1] || arguments[1] === 0) { score += arguments[1]; this.score = score; if (arguments[3]) { return this.score } }
-        if (arguments[2] || arguments[2] === 0) { bombs += arguments[2]; this.bombs = bombs; if (arguments[3]) { return this.bombs } }
-    }
+    this.event = function () {
+        //修改属性值 lives score bombs //闭包
+        if (arguments[0] || arguments[0] === 0) {
+            lives += arguments[0];
+            this.lives = lives;
+            if (arguments[3]) {
+                return this.lives;
+            }
+        }
+        if (arguments[1] || arguments[1] === 0) {
+            score += arguments[1];
+            this.score = score;
+            if (arguments[3]) {
+                return this.score;
+            }
+        }
+        if (arguments[2] || arguments[2] === 0) {
+            bombs += arguments[2];
+            this.bombs = bombs;
+            if (arguments[3]) {
+                return this.bombs;
+            }
+        }
+    };
     this.move = function (dr, dl, dd, dt) {
         var elm_sl = parseFloat(this.element.style.left),
             elm_st = parseFloat(this.element.style.top);
-        if (elm_st < 0) { dt = 0 } //上
-        else if (elm_st > body_ch - 20) { dd = 0 } //下
-        if (elm_sl < 0) { dl = 0 } //左
-        else if (elm_sl > body_cw - 20) { dr = 0 } //右
+        if (elm_st < 0) {
+            dt = 0;
+        } //上
+        else if (elm_st > body_ch - 20) {
+            dd = 0;
+        } //下
+        if (elm_sl < 0) {
+            dl = 0;
+        } //左
+        else if (elm_sl > body_cw - 20) {
+            dr = 0;
+        } //右
         this.element.style.left = parseFloat(this.element.style.left) + spd * (dr - dl) + 'px';
         this.element.style.top = parseFloat(this.element.style.top) + spd * (dd - dt) + 'px';
-    }
+    };
     this.fire = function (isfire) {
-        if (isfire && bomb_time >= 180) { //15帧 4 bullet => 1秒 16 bullet
+        if (isfire && bomb_time >= 180) {
+            //15帧 4 bullet => 1秒 16 bullet
             fast = () => {
-                player.multi_generator(15, [[7, 0], [-7, 0]], [[0, -10], [0, -10]], [['fire']], [], '', 2);
-                player.multi_generator(15, [[14, 0], [-14, 0]], [[1, -5], [-1, -5]], [['fire', true]], [], '', 2);
-            }
+                player.multi_generator(
+                    15,
+                    [
+                        [7, 0],
+                        [-7, 0],
+                    ],
+                    [
+                        [0, -10],
+                        [0, -10],
+                    ],
+                    [['fire']],
+                    [],
+                    '',
+                    2,
+                );
+                player.multi_generator(
+                    15,
+                    [
+                        [14, 0],
+                        [-14, 0],
+                    ],
+                    [
+                        [1, -5],
+                        [-1, -5],
+                    ],
+                    [['fire', true]],
+                    [],
+                    '',
+                    2,
+                );
+            };
             slow = () => {
-                player.multi_generator(15, [[7, 0], [-7, 0]], [[0, -10], [0, -10]], [['fire']], [], '', 2);
-                player.multi_generator(15, [[14, 0], [-14, 0]], [[0, -10], [0, -10]], [['fire']], [], '', 2);
-            }
+                player.multi_generator(
+                    15,
+                    [
+                        [7, 0],
+                        [-7, 0],
+                    ],
+                    [
+                        [0, -10],
+                        [0, -10],
+                    ],
+                    [['fire']],
+                    [],
+                    '',
+                    2,
+                );
+                player.multi_generator(
+                    15,
+                    [
+                        [14, 0],
+                        [-14, 0],
+                    ],
+                    [
+                        [0, -10],
+                        [0, -10],
+                    ],
+                    [['fire']],
+                    [],
+                    '',
+                    2,
+                );
+            };
             spd == 2 ? slow() : fast();
         }
-    }
-    this.fire_bomb = function (isbomb) { //bomb 144伤害
-        if (bomb_time < 180) { //恢复bomb
+    };
+    this.fire_bomb = function (isbomb) {
+        //bomb 144伤害
+        if (bomb_time < 180) {
+            //恢复bomb
             bomb_time++;
-            bombs_elm.children[1].style.width = bomb_time / 180 * 530 + 'px ';
-            this.multi_generator(5, [[14, 0], [-14, 0], [7, 0], [-7, 0]], [[1, -5], [-1, -5], [0, -10], [0, -10]], [['fire', true]], [], '', 4);
+            bombs_elm.children[1].style.width = (bomb_time / 180) * 530 + 'px ';
+            this.multi_generator(
+                5,
+                [
+                    [14, 0],
+                    [-14, 0],
+                    [7, 0],
+                    [-7, 0],
+                ],
+                [
+                    [1, -5],
+                    [-1, -5],
+                    [0, -10],
+                    [0, -10],
+                ],
+                [['fire', true]],
+                [],
+                '',
+                4,
+            );
             if (bomb_time == parseInt(res_time) && res_switch) {
                 res_maker.obj_type = 'res_bomb';
                 res_maker.generator(1, [Math.random() * 530, -25], [0, 1]);
                 res_switch = false;
             }
-        } else if (isbomb && this.bombs > 0) { //用bomb
+        } else if (isbomb && this.bombs > 0) {
+            //用bomb
             this.event('', '', -1);
             bomb_time = 0;
             this.invincible_time = 0;
             bullet_decrease = true;
             res_time = Math.random() * 180;
-            for (i of res_list) { i.move_type = ['absorb', 500] } //回收资源
-        } else { bullet_decrease = false; }
-    }
+            for (i of res_list) {
+                i.move_type = ['absorb', 500];
+            } //回收资源
+        } else {
+            bullet_decrease = false;
+        }
+    };
     this.crash = function (i) {
-        if (distance(this.element, i.element) < i.size[0] / 2 + 2) { //判定点 4*4
+        if (distance(this.element, i.element) < i.size[0] / 2 + 2) {
+            //判定点 4*4
             if (i.type == 'bullet_boss') i.lives--;
-            if (this.invincible_time == 180) { //被撞
+            if (this.invincible_time == 180) {
+                //被撞
                 if (this.lives > 0) {
                     this.event(-1);
                     this.no_died = false;
@@ -76,25 +188,40 @@ function Player(x, y) {
             res_maker.generator(1, [Math.random() * 530, -25], [0, 1]);
             res_switch = false;
         }
-    }
-    this.wipe = function (i) { //擦弹事件
+    };
+    this.wipe = function (i) {
+        //擦弹事件
         var dis = distance(this.element, i.element);
-        if (dis < i.size[0] / 2 + 10) { //身体 20*20
+        if (dis < i.size[0] / 2 + 10) {
+            //身体 20*20
             switch (i.type) {
-                case 'boss': break;
-                case 'bullet_boss': this.event('', 1); break;
-                case 'res_live': i.lives = 0; if (this.lives < 5) this.event(1); break;
-                case 'res_bomb': i.lives = 0; if (this.bombs < 5) this.event('', '', 1); break;
-                case 'res_score': this.event('', i.lives); i.lives = 0; break;
+                case 'boss':
+                    break;
+                case 'bullet_boss':
+                    this.event('', 1);
+                    break;
+                case 'res_live':
+                    i.lives = 0;
+                    if (this.lives < 5) this.event(1);
+                    break;
+                case 'res_bomb':
+                    i.lives = 0;
+                    if (this.bombs < 5) this.event('', '', 1);
+                    break;
+                case 'res_score':
+                    this.event('', i.lives);
+                    i.lives = 0;
+                    break;
             }
-        } else if (dis < i.size[0] / 2 + 30) { //身体 往外20
+        } else if (dis < i.size[0] / 2 + 30) {
+            //身体 往外20
             if (i.type.includes('boss') && i.generate_num < 10) {
                 i.obj_type = 'res_score';
                 i.generator(30, [], [0.2, 0], [['gravity', 0.005]], [], ['ring', 4]);
                 i.obj_type = 'bullet_boss';
             }
         }
-    }
+    };
     this.draw = function () {
         //判定点
         var hit_point_elm = document.createElement('div');
@@ -112,6 +239,6 @@ function Player(x, y) {
         this.element.style.height = '20px';
         this.element.style.backgroundColor = '#cc0000cc';
         document.body.insertBefore(this.element, document.querySelector('script'));
-    }
+    };
 }
 Player.prototype = new Maker();
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function Unit(elm) {
     this.elm.style.fontSize = Math.random() * 30 + 30 + 'px';
     this.width = parseFloat(window.getComputedStyle(elm)['width']);
     this.height = parseFloat(window.getComputedStyle(elm)['height']);
-    this.elm.style.top = Math.random() * (body_height - this.height) * 2 / 3 + 50 + 'px';
+    this.elm.style.top = (Math.random() * (body_height - this.height) * 2) / 3 + 50 + 'px';
     this.elm.style.left = Math.random() * (body_width - this.width) + 'px';
     this.spd = [0, 0];
     this.stop = false;
@@ -15,13 +15,17 @@ Unit.prototype.position = function () {
     this.right = this.left + this.width;
     this.top = parseFloat(window.getComputedStyle(this.elm)['top']);
     this.bottom = this.top + this.height;
-}
+};
 Unit.prototype.achieve = function () {
     //运动
     var obj = this,
-        spd = this.spd, elm = this.elm,
-        width = this.width, height = this.height;
-    var between = function (x, min, max) { return x > min && x < max; }
+        spd = this.spd,
+        elm = this.elm,
+        width = this.width,
+        height = this.height;
+    var between = function (x, min, max) {
+        return x > min && x < max;
+    };
     var move = function () {
         var left = parseFloat(window.getComputedStyle(elm)['left']),
             right = left + width,
@@ -33,35 +37,51 @@ Unit.prototype.achieve = function () {
                 var to_top = () => {
                     if (i.stop) spd[1] = -spd[1] * damp;
                     else {
-                        if (spd[1] - i.spd[1] < 0) { var vh = spd[1]; spd[1] = i.spd[1] * damp; i.spd[1] = vh * damp; }
+                        if (spd[1] - i.spd[1] < 0) {
+                            var vh = spd[1];
+                            spd[1] = i.spd[1] * damp;
+                            i.spd[1] = vh * damp;
+                        }
                         i.elm.style.top = i.top - (i.bottom - top) * 1 + 'px'; //向上
                     }
                     elm.style.top = top + (i.bottom - top) * 1 + 'px'; //向下
-                }
+                };
                 var to_bottom = () => {
                     if (i.stop) spd[1] = -spd[1] * damp;
                     else {
-                        if (spd[1] - i.spd[1] > 0) { var vh = spd[1]; spd[1] = i.spd[1] * damp; i.spd[1] = vh * damp; }
+                        if (spd[1] - i.spd[1] > 0) {
+                            var vh = spd[1];
+                            spd[1] = i.spd[1] * damp;
+                            i.spd[1] = vh * damp;
+                        }
                         i.elm.style.top = i.top + (bottom - i.top) * 1 + 'px';
                     }
                     elm.style.top = top - (bottom - i.top) * 1 + 'px';
-                }
+                };
                 var to_left = () => {
                     if (i.stop) spd[0] = -spd[0] * damp;
                     else {
-                        if (spd[0] - i.spd[0] < 0) { var vw = spd[0]; spd[0] = i.spd[0] * damp; i.spd[0] = vw * damp; }
+                        if (spd[0] - i.spd[0] < 0) {
+                            var vw = spd[0];
+                            spd[0] = i.spd[0] * damp;
+                            i.spd[0] = vw * damp;
+                        }
                         i.elm.style.left = i.left - (i.right - left) * 1 + 'px'; //向左
                     }
                     elm.style.left = left + (i.right - left) * 1 + 'px'; //向右
-                }
+                };
                 var to_right = () => {
                     if (i.stop) spd[0] = -spd[0] * damp;
                     else {
-                        if (spd[0] - i.spd[0] > 0) { var vw = spd[0]; spd[0] = i.spd[0] * damp; i.spd[0] = vw * damp; }
+                        if (spd[0] - i.spd[0] > 0) {
+                            var vw = spd[0];
+                            spd[0] = i.spd[0] * damp;
+                            i.spd[0] = vw * damp;
+                        }
                         i.elm.style.left = i.left + (right - i.left) * 1 + 'px';
                     }
                     elm.style.left = left - (right - i.left) * 1 + 'px';
-                }
+                };
 
                 i.position();
                 var crs_left = between(left, i.left, i.right),
@@ -76,21 +96,29 @@ Unit.prototype.achieve = function () {
                     if (crs_left) to_left(); //左包含
                     if (crs_right) to_right(); //右包含
                 }
-                if (crs_left && crs_top) { //左上角重合
+                if (crs_left && crs_top) {
+                    //左上角重合
                     if (crs_bottom) undefined; //左被包含
-                    else if (i.right - left > i.bottom - top) to_top(); else to_left();
+                    else if (i.right - left > i.bottom - top) to_top();
+                    else to_left();
                 }
-                if (crs_right && crs_top) { //右上角重合
+                if (crs_right && crs_top) {
+                    //右上角重合
                     if (crs_left) undefined; //上被包含
-                    else if (right - i.left > i.bottom - top) to_top(); else to_right();
+                    else if (right - i.left > i.bottom - top) to_top();
+                    else to_right();
                 }
-                if (crs_left && crs_bottom) {  //左下角重合
+                if (crs_left && crs_bottom) {
+                    //左下角重合
                     if (crs_right) undefined; //下被包含
-                    else if (i.right - left > bottom - i.top) to_bottom(); else to_left();
+                    else if (i.right - left > bottom - i.top) to_bottom();
+                    else to_left();
                 }
-                if (crs_right && crs_bottom) { //右下角重合
+                if (crs_right && crs_bottom) {
+                    //右下角重合
                     if (crs_top) undefined; //右被包含
-                    else if (right - i.left > bottom - i.top) to_bottom(); else to_right();
+                    else if (right - i.left > bottom - i.top) to_bottom();
+                    else to_right();
                 }
             }
         }
@@ -105,9 +133,17 @@ Unit.prototype.achieve = function () {
             }
         }
         //body碰撞
-        if (left < 10 && spd[0] <= 0) { spd[0] = -spd[0] * damp; elm.style.left = '10px'; }
-        if (right > body_width - 10 && spd[0] >= 0) { spd[0] = -spd[0] * damp; elm.style.left = body_width - width - 10 + 'px'; }
-        if (top < -200 && spd[1] <= 0) { elm.style.top = body_height - height - 0 + 'px'; }
+        if (left < 10 && spd[0] <= 0) {
+            spd[0] = -spd[0] * damp;
+            elm.style.left = '10px';
+        }
+        if (right > body_width - 10 && spd[0] >= 0) {
+            spd[0] = -spd[0] * damp;
+            elm.style.left = body_width - width - 10 + 'px';
+        }
+        if (top < -200 && spd[1] <= 0) {
+            elm.style.top = body_height - height - 0 + 'px';
+        }
         if (bottom > body_height - 20) {
             if (spd[1] >= 0) spd[1] = -spd[1] * damp;
             if (Math.abs(spd[1]) < 0.1) {
@@ -123,14 +159,16 @@ Unit.prototype.achieve = function () {
         // debugger;
         if (url_lic) obj.stop = true;
         requestAnimationFrame(move);
-    }
+    };
     requestAnimationFrame(move);
     //事件
     var url = this.url;
-    this.elm.onmousedown = function (e) { //拖拽事件
+    this.elm.onmousedown = function (e) {
+        //拖拽事件
         var ismove = false;
         obj.stop = true;
-        spd[0] = 0; spd[1] = 0;
+        spd[0] = 0;
+        spd[1] = 0;
         var ex = e.clientX || e.touches[0].clientX,
             ey = e.clientY || e.touches[0].clientY;
         var px = parseFloat(elm.style.left),
@@ -140,57 +178,76 @@ Unit.prototype.achieve = function () {
         document.onmousemove = function (e) {
             var ex = e.clientX || e.touches[0].clientX,
                 ey = e.clientY || e.touches[0].clientY;
-            var vx = e.movementX, v_limit = 10,
+            var vx = e.movementX,
+                v_limit = 10,
                 vy = e.movementY;
             if (vx === undefined) vx = Math.random() * 20 - 10;
-            else if (vx > v_limit) vx = v_limit; else if (vx < -v_limit) vx = -v_limit;
+            else if (vx > v_limit) vx = v_limit;
+            else if (vx < -v_limit) vx = -v_limit;
             if (vy === undefined) vy = Math.random() * 20 - 10;
-            else if (vy > v_limit) vy = v_limit; else if (vy < -v_limit) vy = -v_limit;
-            spd[0] = vx; spd[1] = vy;
+            else if (vy > v_limit) vy = v_limit;
+            else if (vy < -v_limit) vy = -v_limit;
+            spd[0] = vx;
+            spd[1] = vy;
             if (ex + dx > 10 && ex + dx < body_width - width - 10) elm.style.left = dx + ex + 'px';
             if (ey + dy < body_height - height - 20) elm.style.top = dy + ey + 'px';
             ismove = true;
-        }
+        };
         elm.onmouseup = function () {
             document.onmousemove = null;
-            document.ontouchmove = function () { return false };
+            document.ontouchmove = function () {
+                return false;
+            };
             obj.stop = !obj.stop;
             if (url_lic && !ismove) window.open(url, '_self');
-        }
+        };
         document.ontouchmove = document.onmousemove;
         elm.ontouchend = elm.onmouseup;
-    }
-    this.elm.oncontextmenu = function () { obj.stop = true; }
+    };
+    this.elm.oncontextmenu = function () {
+        obj.stop = true;
+    };
     this.elm.ontouchstart = this.elm.onmousedown;
-}
+};
 
 //说明
 var info = document.getElementById('info');
-info.onclick = () => { info.style.display = 'none' };
+info.onclick = () => {
+    info.style.display = 'none';
+};
 info.ontouchstart = info.onclick;
 // info.onclick(); //debug
 //环境参数
 var body = document.getElementById('body');
 var body_width = parseFloat(window.getComputedStyle(body)['width']),
     body_height = parseFloat(window.getComputedStyle(body)['height']);
-var elms = document.getElementsByClassName('unit'), Units = [];
-var gravity, f_air, f_ground, damp, vmax = 2;
+var elms = document.getElementsByClassName('unit'),
+    Units = [];
+var gravity,
+    f_air,
+    f_ground,
+    damp,
+    vmax = 2;
 var url_lic = true;
 //设置面板
 var btn = document.getElementById('btn');
 var option = document.getElementById('option');
 option.children[1].children[2].onchange = function () {
-    this.previousElementSibling.innerHTML = (this.value * 100).toFixed(2); gravity = parseFloat(this.value);
-}
+    this.previousElementSibling.innerHTML = (this.value * 100).toFixed(2);
+    gravity = parseFloat(this.value);
+};
 option.children[2].children[2].onchange = function () {
-    this.previousElementSibling.innerHTML = (this.value * 1e+2).toFixed(2); f_air = parseFloat(this.value);
-}
+    this.previousElementSibling.innerHTML = (this.value * 1e2).toFixed(2);
+    f_air = parseFloat(this.value);
+};
 option.children[3].children[2].onchange = function () {
-    this.previousElementSibling.innerHTML = (this.value * 1e+3).toFixed(2); f_ground = parseFloat(this.value);
-}
+    this.previousElementSibling.innerHTML = (this.value * 1e3).toFixed(2);
+    f_ground = parseFloat(this.value);
+};
 option.children[4].children[2].onchange = function () {
-    this.previousElementSibling.innerHTML = (this.value * 1).toFixed(2); damp = parseFloat(this.value);
-}
+    this.previousElementSibling.innerHTML = (this.value * 1).toFixed(2);
+    damp = parseFloat(this.value);
+};
 for (var child of option.children) if (child.children[2]) child.children[2].onchange();
 option.children[0].children[1].onclick = function () {
     url_lic = !url_lic;
@@ -198,39 +255,39 @@ option.children[0].children[1].onclick = function () {
         this.style.backgroundColor = '#fbb';
         this.style.color = '#fff';
         this.value = 'ON';
-    }
-    else {
+    } else {
         this.style.backgroundColor = '#fff';
         this.style.color = '#fbb';
         this.value = 'OFF';
     }
-}
+};
 option.onanimationend = function () {
     if (this.style.animationDirection == 'reverse') this.style.display = 'none';
     this.style.animation = '';
-}
+};
 btn.onclick = function () {
     if (option.style.display == 'flex') {
         option.style.animation = 'slide 0.4s ease-out';
         option.style.animationDirection = 'reverse';
         body.onclick = null;
-    }
-    else {
+    } else {
         option.style.display = 'flex';
         option.style.animation = 'slide 0.4s ease-out';
         body.onclick = btn.onclick;
     }
-}
+};
 // option.children[0].children[1].onclick(); //debug
 //功能
 for (var elm of elms) Units.push(new Unit(elm));
 for (var unit of Units) unit.achieve();
-document.oncontextmenu = function () { return false }
+document.oncontextmenu = function () {
+    return false;
+};
 //egg
 var egg_key = setInterval(() => {
     var reserve = [];
     for (var unit of Units) {
-        if ((parseFloat(unit.elm.style.top) > body_height - 10) && true) unit.elm.remove();
+        if (parseFloat(unit.elm.style.top) > body_height - 10 && true) unit.elm.remove();
         else reserve.push(unit);
     }
     Units = reserve;
@@ -238,7 +295,9 @@ var egg_key = setInterval(() => {
         info.innerText = '如果你看到这句话\n说明你发现了\n...';
         info.style.display = 'flex';
         info.style.animation = 'slide 4s cubic-bezier(0, 0.3, 0.2, 1)';
-        info.onanimationend = function () { info.innerText = info.innerText.replace('...', 'BUG'); }
+        info.onanimationend = function () {
+            info.innerText = info.innerText.replace('...', 'BUG');
+        };
         clearInterval(egg_key);
     }
 }, 5000);
